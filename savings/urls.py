68c5ee15b27c25cc13b1d5af95496e5431357b7b
akashg001 <akash.gupta4701@gmail.com>
2022-02-19 11:54:05 +0530

    .

--- savings/urls.py
+++ savings/urls.py
@@ -14,4 +14,4 @@ urlpatterns = [
     url(r'^media/(?p<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^media/(?p<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),  
 ]
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,documet_root=settings.MEDIA_ROOT)
