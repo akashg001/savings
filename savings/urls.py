bbd71c09c35a348c1f59393114f2553cd9ce60af
akashg001 <akash.gupta4701@gmail.com>
2022-02-19 13:27:25 +0530

    update

--- savings/urls.py
+++ savings/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.config.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
